648c1dcd092f8c7d8c542575bd1a8b7e03ac0f79
Leekyoeun <44278287+Leekyoeun@users.noreply.github.com>
2020-04-29 05:42:05 +0900

    C# 기본

--- CBasic/Program.cs
+++ CBasic/Program.cs
@@ -10,6 +10,33 @@ namespace CBasic
     {
         static void Main(string[] args)
         {
+            var name = "이" + "교" + "은";
+            Console.Write("Hello C#");
+            int add= 2;
+            int Alpha;
+            //주석
+            /*
+             * 여러줄 주석
+             */
+            Console.WriteLine("Hello World"); //행 바꿈
+            Console.Write("Hello World");//행 안바꿈
+            Console.Write("hello world");
+
+            Console.Write(52);
+            Console.WriteLine(7 % 3);
+            Console.WriteLine('A');
+            Console.WriteLine('A');
+
+            Console.WriteLine("한빛\t아카데미");
+            Console.WriteLine("한빛\n아카데미");
+            Console.WriteLine("\"\"\"");
+            Console.WriteLine("가" + "나" + "다");
+
+            Console.WriteLine("안녕"[100]);
+
+            Console.WriteLine(true);
+            Console.WriteLine(true);
+
         }
     }
 }
